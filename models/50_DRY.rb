bb32c1400ce62cb969cf318c827fefb1fec48e5d
ejaramillom <ejaramilo@hotmail.com>
2024-02-13 21:21:40 +0000

    50 dry

--- models/50_DRY.rb
+++ models/50_DRY.rb
@@ -76,3 +76,46 @@ end
 Dir[File.join(Rails.root, 'lib', '*.rb')].each do |module|
   require module
 end
+
+# plugin
+
+./script/rails generate plugin drivable
+
+# http://guides.rubyonrails.org/plugins.html.
+
+# lib/drivable/active_record_extensions.rb
+module Drivable
+  module ActiveRecordExtensions
+    module ClassMethods
+      def drivable
+        validates_presence_of :direction, :speed
+        
+        include ActiveRecordExtensions::InstanceMethods
+      end
+    end
+  
+    module InstanceMethods
+      def turn(new_direction)
+        self.direction = new_direction
+      end
+      
+      def brake
+        self.speed = 0
+      end
+      
+      def accelerate
+        self.speed = [speed + 10, 100].min
+      end
+    end
+  end
+end
+
+# lib/drivable.rb
+require "drivable/active_record_extensions"
+  
+class ActiveRecord::Base
+  extend Drivable::ActiveRecordExtensions::ClassMethods
+end
+  
+# init.rb
+require File.join(File.dirname(__FILE__), "lib", "drivable")
