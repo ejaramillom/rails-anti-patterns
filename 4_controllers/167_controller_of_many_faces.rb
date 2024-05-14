737beae6cc717097d6716528e24878ae1c38af5c
ejaramillom <ejaramilo@hotmail.com>
2024-05-14 19:04:43 +0000

    167 controller of many faces

--- 4_controllers/167_controller_of_many_faces.rb
+++ 4_controllers/167_controller_of_many_faces.rb
@@ -21,3 +21,36 @@ class UsersController < ApplicationController
   
   ... RESTful actions ...
 end
+
+# better
+# define the resources
+resource :sessions, :only => [:new, :create, :destroy]
+match "/login" => "user_sessions#new", :as => :login
+match "/logout" => "user_sessions#destroy", :as => :logout
+
+# set the behaviors independently
+
+class SessionsController < ApplicationController
+  def new
+    # Just render the sessions/new.html.erb template
+  end
+    
+  def create
+    if session[:user_id] = User.authenticate(params[:user][:login],
+      params[:user][:password])
+      flash[:message] = "Login successful"
+      redirect_to root_url
+    else
+      flash.now[:warning] = "Login unsuccessful"
+      render :action => "new"
+    end
+  end
+    
+  def destroy
+    session[:user_id] = nil
+    flash[:message] = 'Logged out'
+    redirect_to login_url
+  end
+end
+
+# You see, new and edit are not really RESTful actions. At its core, REST asks for only the index , create , show , update , and destroy actions.
